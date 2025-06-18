8e7baf39fd92a211b8b99b490453ef5874320908
divyanshuthakur14 <singhthakurdivyanshu@gmail.com>
2025-06-18 15:56:47 +0530

    mvcPattern

---
+++ Design Patterns and Principles/MVCPatternExample/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        Student student = new Student("S101", "Anshul", "A");
+        StudentView view = new StudentView();
+        StudentController controller = new StudentController(student, view);
+
+        controller.updateView(); 
+
+        controller.setStudentName("Divyanshu");
+        controller.setStudentGrade("B+");
+
+        controller.updateView(); 
+    }
+}
